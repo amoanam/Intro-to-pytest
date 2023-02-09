66ec08380268ecf82f33f916f9bb5f28f63aef71
Abdul Moanam <109928376+amoanam@users.noreply.github.com>
2023-02-09 22:22:19 +0100

    Add files via upload

---
+++ test_math.py
@@ -0,0 +1,12 @@
+""""
+This module contains basic unit tests for math operations.
+Their purpose is to show you how to use pytest
+"""
+
+# A most basic test function
+
+def test_one_plus_one ():
+    assert 1 +1 == 2
+    
+    
+    
