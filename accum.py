78944fe1a41ac6e3ef7c3b70b9a5b24206475f9e
Abdul Moanam <109928376+amoanam@users.noreply.github.com>
2023-02-12 03:48:56 +0100

    Add files via upload

---
+++ accum.py
@@ -0,0 +1,18 @@
+"""
+This module contains a basic accumulator class.
+its main purpose is to show how to use pytest framework for testing classes
+"""
+
+# Accumulator class
+
+class Accumulator:
+    
+    def _init_(self):
+        self.count = 0
+        
+@property
+def count(self):
+    return self._count
+
+def add(self, more=1):
+    self._count += more
